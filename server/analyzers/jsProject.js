79767fd1f2c7738b3fed16d37f907466f04e2e6d
suyog-basnet <suyogbasnet2072g@gmail.com>
2026-09-01 16:48:31 +0545

    feat: wire dead-code, file-stats, and complexity analysis into jsProject analyzer

--- server/analyzers/jsProject.js
+++ server/analyzers/jsProject.js
@@ -1,12 +1,15 @@
 import { loadProject } from "../lib/parseProject.js";
 import { buildDependencyGraph } from "../lib/buildGraph.js";
+import { findDeadCode } from "../lib/deadCode.js";
 
 export function analyzeJsProject(rootDir) {
   const project = loadProject(rootDir);
   const graph = buildDependencyGraph(project, rootDir);
+  const deadCode = findDeadCode(graph);
 
   return {
     fileCount: project.getSourceFiles().length,
     graph,
+    deadCode,
   };
 }
